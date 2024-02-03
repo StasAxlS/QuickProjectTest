3f510b4620b36f16af0eca708f7db1e8dc65b129
StasAxlS <130902136+StasAxlS@users.noreply.github.com>
2024-02-03 17:42:29 +0300

    Initial commit

---
+++ src/main/java/com/example/quickprojecttest/QuickProjectTestApplication.java
@@ -0,0 +1,13 @@
+package com.example.quickprojecttest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuickProjectTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuickProjectTestApplication.class, args);
+	}
+
+}
